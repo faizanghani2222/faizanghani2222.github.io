ec31fd58f2b900ef421b21f7b103931e51bdd5ed
Faizan Ghani <faizanghani2222@gmail.com>
2022-10-19 17:57:25 +0530

    updated projects

--- src/Container/Projects.jsx
+++ src/Container/Projects.jsx
@@ -2,7 +2,7 @@ import { Box, Button, Flex, Heading, Image, Link, ListItem, Text, UnorderedList 
 import React from 'react';
 import Fade from 'react-reveal/Fade'; 
 import zara from "../images/zara.png"
-import dh from "../images/dh.png"
+import gh from "../images/gh.png"
 import jio from "../images/jio.png"
 import GitHubCalendar from 'react-github-calendar';
 function Projects(){
@@ -21,7 +21,47 @@ function Projects(){
             <Heading fontWeight="600" fontSize="16px" w="fit-content" px="10px" py="5px" m="auto" background="rgb(245, 223, 78)">Projects</Heading>
             <Heading fontSize="32px" px="10%" textAlign="center">Some of my recent projects</Heading>
             <Box mt="3%" pb="30px" px="10%">
-                <Fade left cascade>
+               
+            <Fade left cascade>
+                <Flex  wrap={{base:"wrap-reverse",md:"nowrap",lg:"nowrap"}} gap={"10px"} mb="20px" px="30px" py="20px"  boxShadow={"rgb(245, 223, 78) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px"} justify="space-between">
+                    <Box>
+                    <Heading fontSize={["20px","28px","28px"]}>Harvest- Clone (Group project)</Heading>
+                    <Text as="b">Tech Stack:</Text><Text>React | Chakra UI | Firebase | NodeJs | Express | MongoDB |
+JavaScript | HTML | CSS</Text>
+                    <Text display="block" as="b">Features:</Text>
+                    <UnorderedList>
+                        <ListItem>Live time track feature connected by our backend to track time</ListItem>
+                        <ListItem>User can create Invoice and download it as pdf</ListItem>
+                        <ListItem>Responsive and mobile-friendly website</ListItem>
+                        <ListItem>User can manage projects, add update, delete or export it</ListItem>
+                   </UnorderedList>
+                  
+                    <Text display="block" as="b">Area of Responsibility</Text>
+                    <UnorderedList>
+                        <ListItem>Time track section and its backend</ListItem>
+                        <ListItem>Invoice section and its backend</ListItem>
+                   </UnorderedList>
+                   <Flex mt="1%" justify="space-between" alignItems="center">
+                   <Link href="https://get-harvest-rct201clone.netlify.app/" isExternal>
+                    <Button background="rgb(140, 78, 255)" color="white">Live 
+                    <i style={{marginLeft:"6px"}} class="fa-sharp fa-solid fa-arrow-up-right-from-square"></i>
+                    </Button>
+                    </Link>
+                    <Link href="https://github.com/SHARUN266/Get-Harvest-clone" isExternal>
+                    <Button background="rgb(140, 78, 255)" color="white">Code
+                    <i style={{marginLeft:"6px"}} class="fa-brands fa-github"></i>
+                    </Button>
+                    </Link>
+                   </Flex>
+                    </Box>
+                    <Box w={["100%","100%","50%","50%"]}>
+                        <Image transition="transform .2s" _hover={{transform:" scale(1.2)"}} w="100%" h={["200px","100%%","100%"]} src={gh}/>
+                    </Box>
+                </Flex>
+                </Fade>
+               
+               
+                <Fade right cascade>
                 <Flex  wrap={{base:"wrap-reverse",md:"nowrap",lg:"nowrap"}} gap={"10px"} mb="20px" px="30px" py="20px"  boxShadow={"rgb(245, 223, 78) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px"} justify="space-between">
                     <Box py="5px">
                     <Heading fontSize={["20px","28px","28px"]}>Zara - Clone (Individual-Project)</Heading>
@@ -54,7 +94,7 @@ function Projects(){
                 </Flex>
                 </Fade>
 
-                <Fade right cascade>
+                <Fade left cascade>
                 <Flex  wrap={{base:"wrap-reverse",md:"nowrap",lg:"nowrap"}} gap={"10px"} mb="20px" px="30px" py="20px"  boxShadow={"rgb(245, 223, 78) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px"} justify="space-between">
                     <Box>
                     <Heading fontSize={["20px","28px","28px"]}>Jio Mart - Clone (Group-Project)</Heading>
@@ -89,38 +129,7 @@ function Projects(){
                 </Flex>
                 </Fade>
 
-                <Fade left cascade>
-                <Flex  wrap={{base:"wrap-reverse",md:"nowrap",lg:"nowrap"}} gap={"10px"} mb="20px" px="30px" py="20px"  boxShadow={"rgb(245, 223, 78) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px"} justify="space-between">
-                    <Box>
-                    <Heading fontSize={["20px","28px","28px"]}>Deccan Herald - Clone (Group project)</Heading>
-                    <Text as="b">Tech Stack:</Text><Text> HTML | CSS | JavaScript</Text>
-                    <Text display="block" as="b">Features:</Text>
-                    <UnorderedList>
-                        <ListItem>A complete news website having different sections and categories</ListItem>
-                   </UnorderedList>
-                    <Text display="block" as="b">Area of Responsibility</Text>
-                    <UnorderedList>
-                        <ListItem>Layout for all categories page and news details page</ListItem>
-                        <ListItem>some static pages</ListItem>
-                   </UnorderedList>
-                   <Flex mt="1%" justify="space-between" alignItems="center">
-                   <Link href="https://enchanting-muffin-9f8441.netlify.app/" isExternal>
-                    <Button background="rgb(140, 78, 255)" color="white">Live 
-                    <i style={{marginLeft:"6px"}} class="fa-sharp fa-solid fa-arrow-up-right-from-square"></i>
-                    </Button>
-                    </Link>
-                    <Link href="https://github.com/Sakil9051/WEB-101-Project" isExternal>
-                    <Button background="rgb(140, 78, 255)" color="white">Code
-                    <i style={{marginLeft:"6px"}} class="fa-brands fa-github"></i>
-                    </Button>
-                    </Link>
-                   </Flex>
-                    </Box>
-                    <Box w={["100%","100%","50%","50%"]}>
-                        <Image transition="transform .2s" _hover={{transform:" scale(1.2)"}} w="100%" h={["200px","100%%","100%"]} src={dh}/>
-                    </Box>
-                </Flex>
-                </Fade>
+               
                 <Fade right cascade>
                 <Box mt={["15%","7%"]}>
 
